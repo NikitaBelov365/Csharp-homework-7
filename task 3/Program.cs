fbd33e83f62673e2f0e97cd8d6b19e6a6ce68032
Nikitabelov365 <nikitabelov365@gmail.com>
2023-03-04 23:00:29 +0300

    task 3 solved

---
+++ task 3/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 3: Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int InputSize(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] ArrayCreation(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+void ArithmeticalMean(int[,] array)
+{
+    
+    for (int j = 0; j < array.GetLength(0); j++)
+        {      
+        int sum = 0;  
+        int average = 0;
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            sum += array[i,j];
+        }
+        average = sum/array.GetLength(1);
+        System.Console.WriteLine($"Average in {j+1} column is {average}");
+
+    }
+}
+
+int rows = InputSize("Input ammount of rows");
+int columns = InputSize("Input ammount of columns");
+int[,] array = ArrayCreation(rows, columns);
+PrintArray(array);
+ArithmeticalMean(array);
