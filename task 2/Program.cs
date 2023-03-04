8de1dfee7fa711788f005a40905134b32959cf55
Nikitabelov365 <nikitabelov365@gmail.com>
2023-03-04 20:24:22 +0300

    task 2 solved

---
+++ task 2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int InputSize(string msg)
+{
+    System.Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] ArrayCreation(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+int UnEvenSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i+=2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int size = InputSize("Input size");
+int[] array = ArrayCreation(size);
+int sum = UnEvenSum(array);
+PrintArray(array);
+System.Console.WriteLine($"Sum of not even elements is equal: {sum}");
